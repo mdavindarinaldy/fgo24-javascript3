09f9778501ff1611c756e7c1decb31f3ea8a5d4d
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-04-21 17:53:23 +0700

    add the function without using built-in method

--- minitask4.js
+++ minitask4.js
@@ -5,6 +5,8 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
+// Built-in Method
+
 function palindrome(text) {
     let newText = text.split("").toReversed().join("")
     if (text == newText ) {console.log("Palindrome")}
@@ -16,10 +18,37 @@ function reverseWord(text) {
     console.log(newText)
 }
 
+// Manual
+
+function palindromeManual(text) {
+    let len = text.length
+    let newText = ""
+    for (let i=len-1; i>=0; i--) {
+        newText += text[i]
+    }
+    if (newText == text) {console.log("Palindrome")}
+    else {console.log("Bukan Palindrome")}
+}
+
+function reversedManual(text) {
+    let newText = ""
+    let temp = ""
+    for (let i=text.length-1; i>=0; i--) {
+        if (text[i] == " ") { 
+            newText += temp + " "
+            temp = ""
+        } else {
+            temp = text[i] + temp
+        }
+    }
+    newText += temp
+    console.log(newText)
+}
+
 rl.question("Masukkan kata yang akan dicek palindrome: ", function(text) {
     palindrome(text)
     rl.question("Masukkan kata yang akan dibalik: ", function(text) {
-        reverseWord(text)
+        reversed(text)
         rl.close()
     } )
 } )
