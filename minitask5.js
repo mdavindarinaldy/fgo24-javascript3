e01594aa22070400d3a0e50a40f18a0dabbd1a98
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-04-21 17:08:44 +0700

    Add files via upload

---
+++ minitask5.js
@@ -0,0 +1,17 @@
+function queue(name, time) {
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            resolve(name)
+        },time)
+    })
+}
+
+queue("John",1500).then(function(result){
+    console.log(result)
+    queue("Ed",2000).then(function(result){
+        console.log(result)
+        queue("Jane",500).then(function(result){
+            console.log(result)
+        })
+    })
+})
