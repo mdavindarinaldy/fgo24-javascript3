e7e0a4e67a2e9c5a0cdd51a13b39be77daf4c895
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-04-22 11:39:42 +0700

    add async wait

--- minitask6.js
+++ minitask6.js
@@ -1,13 +1,31 @@
 const url = "https://jsonplaceholder.typicode.com/users"
 
+async function fetching(url) {
+    try {
+        const result = await fetch(url) // promise
+        const data = await result.json()
+        let listEmail = []
+        data.forEach(item => {
+            let email = item.email
+            let lcEmail = email.toLowerCase()
+            listEmail.push(lcEmail)
+        })
+        console.log(listEmail)
+    } catch(err) {
+        console.log(err)
+    }
+}
+
 fetch(url).then(res => {
     return res.json() 
 }).then(data => {
     let listEmail = []
-    data.forEach(item=> {
+    data.forEach(item => {
         let email = item.email
         let lcEmail = email.toLowerCase()
         listEmail.push(lcEmail)
     })
     console.log(listEmail)
 })
+
+fetching(url)
