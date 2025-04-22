f2608329ea2ccb7c4fb5673cb4e8e06ce31d0001
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-04-22 08:52:32 +0700

    add array of object person and loop every element

--- minitask5.js
+++ minitask5.js
@@ -1,3 +1,14 @@
+const person = [ { 
+        name: "John",
+        time: 1500
+    }, {
+        name: "Ed",
+        time: 2000
+    }, {
+        name: "Jane",
+        time: 500
+    } ]
+
 function queue(name, time) {
     return new Promise((resolve, reject)=>{
         if(time < 0 || time == undefined) {
@@ -10,18 +21,29 @@ function queue(name, time) {
     })
 }
 
-queue("John",1500).then(function(result){
-    console.log(result)
-    queue("Ed",2000).then(function(result){
+for (let i = 0; i < person.length; i++) {
+    queue(person[i].name, person[i].time).then(function(result){
         console.log(result)
-        queue("Jane",500).then(function(result){
-            console.log(result)
-        }).catch(function(result){
-            console.log(result)
-        })
-    }).catch(function(result){
-        console.log(result)
+    }).catch(function(err){
+        console.log(err)
     })
-}).catch(function(result){
-    console.log(result)
-})
+}
+
+// queue("John",1500).then(function(result){
+//     console.log(result)
+//     queue("Ed",2000).then(function(result){
+//         console.log(result)
+//         queue("Jane",500).then(function(result){
+//             console.log(result)
+//         }).catch(function(result){
+//             console.log(result)
+//         })
+//     }).catch(function(result){
+//         console.log(result)
+//     })
+// }).catch(function(result){
+//     console.log(result)
+// })
+
+//console.time
+//console.timeend???
