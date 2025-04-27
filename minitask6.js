f9b7eed6fec9f38fc64ca864f1077d3a82487a8c
mdavindarinaldy <153896812+mdavindarinaldy@users.noreply.github.com>
2025-04-27 11:56:56 +0700

    feat: add lowercase function w/o built-in method

--- minitask6.js
+++ minitask6.js
@@ -30,4 +30,37 @@ fetch(url).then(res => {
     console.log(data.map(item => item.email.toLowerCase()))
 })
 
-fetching(url)
+// Manual
+
+fetch(url).then(res => {
+    return res.json() 
+}).then(data => {
+    let listEmail = []
+    for (let a = 0; a<data.length; a++) {
+        listEmail = [...listEmail, data[a].email]
+    }
+    lowerCase(listEmail)
+})
+
+function lowerCase(data) {
+    const capital = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",'Q','R','S','T','U','V','W','X','Y','Z']
+    const lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",'q','r','s','t','u','v','w','x','y','z']
+    let newData = []
+    for(let i = 0; i<data.length; i++) {
+        let temp = ""
+        for (let j = 0; j<data[i].length; j++) {
+            for(let x = 0; x<capital.length; x++) {
+                if(data[i][j] == capital[x]) {
+                    temp += lowercase[x]
+                }
+            }
+            for (let y = 0; y<lowercase.length; y++) {
+                if(data[i][j] == lowercase[y]) {
+                    temp += lowercase[y]
+                }
+            }
+        }
+        newData[i] = temp
+    }
+    console.log(newData)
+}
